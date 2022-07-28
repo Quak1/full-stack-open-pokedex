b556c88defe69bcde2371167e51b63c3f81e4c39
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-28 14:29:09 -0700

    skip shouldn't trigger

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (_, res) => {
 })
 
 app.get('/version', (_, res) => {
-  res.send('test 11') // change this string to ensure a new version deployed
+  res.send('test 12') // change this string to ensure a new version deployed
 })
 
 app.get('/hello', (_, res) => {
