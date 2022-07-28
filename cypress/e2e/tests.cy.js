20ca9e2a46f89fecbb3b47b0d8bd1214860e1bff
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-27 18:05:58 -0700

    11.9 Simple end to end -tests

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('can visit a pokemon page', function () {
+    cy.visit('http://localhost:5000', { timeout: 120000 })
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
